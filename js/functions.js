bf38a3cf366740b86775972eb47ab74631da6493
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-09-12 11:19:10 -0300

    Corrigindo timer do slider

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+var x = false;
+window.onload = function() {
+	if (window.matchMedia("(orientation: portrait)").matches) {
+		show();
+	}
+	if(innerWidth > 550) {
+		$('#tutorial-1').css("background-image", "url(/img/small - 00-1.png)"); 
+		$('#tutorial-2').css("background-image", "url(/img/small - 00-2.png)"); 
+	}
+}
+
+function show() {
+	if(x == false) {
+		var mensagem = $("#mensagem");
+		mensagem.show();
+		setTimeout(
+			function(){
+				mensagem.hide();
+			}, 5000);
+		x = true;
+	}
+}
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 7000
+    });
+});
